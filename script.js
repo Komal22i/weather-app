b5e700849aa6e89b3896a4641cc58ba3abaa80d6
Komal Kaushik <komalkaushik@Komals-Laptop.local>
2025-05-13 20:40:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const apiKey = "32781d42b9174902926db221c474a2cf"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchBtn = document.querySelector(".search-button")
+const searchBox = document.querySelector(".field")
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`)
+    var data = await response.json()
+
+    document.querySelector(".city").innerHTML = data.name || "Sonipat";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °c";
+
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+
+
+    if(data.weather[0].main === "Clouds"){
+        weatherIcon.src = "./clouds.png"
+    }
+    else if(data.weather[0].main === "Clear"){
+        weatherIcon.src = "./sun.png"
+    }
+    else if(data.weather[0].main === "Rain"){
+        weatherIcon.src = "./rain.png"
+    }
+    else if(data.weather[0].main === "Drizzle"){
+        weatherIcon.src = "./drizzle.png"
+    }
+    else if(data.weather[0].main === "Mist"){
+        weatherIcon.src = "./mist.png"
+    }
+
+    document.querySelector(".temp-box").style.display = "block"
+
+    
+}
+
+searchBtn.addEventListener(("click"),()=>{
+    checkWeather(searchBox.value)
+})
+
+
+checkWeather()
